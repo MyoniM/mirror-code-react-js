1eb2476c13a60fac08ae9be32293351c21b16a3e
MyoniM <se.yonatan.merkebu@gmail.com>
2022-07-31 18:55:28 +0300

    changed be remote url

--- src/configs/socket.js
+++ src/configs/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
+const URL = "https://stormy-ocean-56729.herokuapp.com";
 const initSocket = () => io(URL);
 
 export default initSocket;
